16597ac96bfeb07378b692a89818abbbea4070c5
Corey Atkins <coatk1@umbc.edu>
2023-08-15 13:04:53 -0400

    Create vscode-extension-install.sh

---
+++ Code/User/vscode-extension-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Source(s):
+# * https://stackoverflow.com/questions/58513266/how-to-install-multiple-extensions-in-vscode-using-command-line
+# * https://github.com/rmmgc/vscode-extensions-bulk-install/blob/main/bulk-install.sh
+
+cat extensions.txt | while read extension || [[ -n $extension ]];
+do
+  code --install-extension $extension
+done
